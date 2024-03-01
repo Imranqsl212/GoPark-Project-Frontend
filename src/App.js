3c3f58455010f0534856d17c50f89f91d6823a5f
Sanjar2010 <sanzarkemelov432@gmail.com>
2024-03-01 17:55:21 +0600

    start adding homePage and header

--- src/App.js
+++ src/App.js
@@ -9,16 +9,18 @@ import ChangePassword from "./pages/ChangePasswordPage/ChangePasswordPage.jsx";
 import EmailSubmissionPage from "./pages/ForgotPassword/EmailSetter/EmailSubmissionPage.jsx";
 import OTPVerificationPage from "./pages/ForgotPassword/OtpChecker/OTPVerificationPage.jsx";
 import PasswordResetPage from "./pages/ForgotPassword/PasswordReseter/PasswordResetPage.jsx";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
   const { checkAuthentication } = useAuthentication();
 
-  useEffect(() => {
-    checkAuthentication();
-  }, []);
+  // useEffect(() => {
+  //   checkAuthentication();
+  // }, []);
 
   return (
     <div>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/reg" element={<RegisterPage />} />
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -2,10 +2,13 @@ import Notification from "../../components/Notification/Notifications.jsx";
 import { useState } from "react";
 import { delay } from "../../additionals/delay.js";
 import { useNavigate } from "react-router-dom";
+import homeImg from '../../assets/images/homeImg.png'
+import './Home.scss'
 
 const Home = () => {
   const navigate = useNavigate();
   const [notification, setNotification] = useState(null);
+
   const logout = async () => {
     localStorage.removeItem("tokens");
     localStorage.removeItem("userDetails");
@@ -22,8 +25,18 @@ const Home = () => {
       {notification && (
         <Notification type={notification.type} text={notification.text} />
       )}
-      <h1>Home</h1>
-      <button onClick={logout}>Logout</button>
+      {/* <h1>Home</h1>
+      <button onClick={logout}>Logout</button> */}
+      <div className="home">
+          <div className="home__text">
+            <h3 className="home__title">GoPark - платформа для попуток и для оптимизации парковки.</h3>
+            <p className="home__subtitle">Она предоставляет информацию о наличии свободных парковочных мест и помогает найти попутчиков, делая акцент на безопасности и устойчивости.</p>
+            <button className="home__btn">Искать попутчика</button>
+          </div>
+          <div className="home__img">
+          <img src={homeImg} alt=""/>
+          </div>
+      </div>
     </div>
   );
 };
